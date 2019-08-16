400eed9950f1c252799aa4c4e736dce4d7f72632
ruru-adra <rabiatul.adawiah83@gmail.com>
2019-08-16 17:12:04 +0800

    Update and rename snp_annotation.R to snp_annotation.sh

--- snp_annotation.sh
+++ snp_annotation.sh
@@ -1,24 +1,20 @@
-##snp annotation
-mkdir data/IRGSP_1.0
+###download data snpEff
+java -jar snpEff.jar download -v rice_rap201304
+
+#create rice gene annotation as references
 cat irgsp.gff irgsp.fa > genes.gff
 java -jar snpEff.jar build -gff3 -v IRGSP_1.0
 
-java -jar ../../snpEff.jar -v IRGSP_1.0 ../../../../Data/PhD/3000/sra/sra_black/PULUT_BURUNG/output/snp_p_burung.hardfilter.vcf > ann_snp_pburung.vcf 
+#annotate snp
+java -jar snpEff.jar -v IRGSP_1.0 snp_p_burung.hardfilter.vcf > ann_snp_pburung.vcf 
 
-
-cut -f 3 transcripts.gff | sort | uniq -c
-
-##check vcf format/debugging
+##check vcf format
 java -jar SnpSift.jar vcfCheck ./data/IRGSP_1.0/amurat_annot.vcf 
 
-##many effects per line
+##annotate many effects per line
 java -jar SnpSift.jar extractFields ./data/IRGSP_1.0/amurat_annot.vcf CHROM POS REF ALT "ANN[*].EFFECT" "ANN[*].GENE" > ./data/IRGSP_1.0/split_per_line2.txt
 
 ##one effect per line
 cat ann_snp_pburung.vcf | ../../scripts/vcfEffOnePerLine.pl | java -jar ../../SnpSift.jar extractFields - CHROM POS REF ALT "ANN[*].EFFECT" "ANN[*].GENE" > snp_pburung_gene.multiple.line.txt
 
 
-
-###download data snpEff
-##check the availability of genome database
-java -jar snpEff.jar download -v rice_rap201304
