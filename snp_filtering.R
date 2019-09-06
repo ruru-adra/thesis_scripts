037a6edc5c8a854b90041665b0edecccf14a4e95
ruru-adra <rabiatul.adawiah83@gmail.com>
2019-09-06 15:58:22 +0800

    Update snp_filtering.R

--- snp_filtering.R
+++ snp_filtering.R
@@ -10,17 +10,17 @@ snp_q76<- read.table("./data/finalSNP_q76.txt", header=T, sep="\t")
 bali$diff<- ave(bali$pos, factor(bali$chr), FUN=function(x) c(NA, diff(x)))
 bali_qual<- filter(bali, diff >= 150)
 
+#filter genic snps with diff pos
+g_bali<- filter(g_bali, effect %in% c("missense_variant", "synonymous_variant", "3_prime_UTR_variant",
+                                     "5_prime_UTR_variant", "intron_variant"))
+                
 #select unique & specific column
 a_bali<- unique(select(snp_bali, snp_id, ref,alel))
 a_ph<- unique(select(snp_ph, snp_id, ref,alel))
 a_mr<- unique(select(snp_mrm16, snp_id, ref,alel))
 a_qr<- unique(select(snp_q100, snp_id, ref,alel))
 a_mw<- unique(select(snp_mr297, snp_id, ref,alel))
-a_qw<- unique(select(snp_q76, snp_id, ref,alel))
-
-#filter genic snps with diff pos
-g_bali<- filter(g_bali, effect %in% c("missense_variant", "synonymous_variant", "3_prime_UTR_variant",
-                                     "5_prime_UTR_variant", "intron_variant"))
+a_qw<- unique(select(snp_q76, snp_id, ref,alel))                
 
 #get total snp from 6 local var
 snp<- Reduce(function(x,y) merge(x, y, by = "snp_id", all.x = TRUE, all.y = TRUE),
@@ -52,11 +52,6 @@ uq_q100<- filter(pv_q100, q100 != "nc")
 uq_mr297<- filter(pv_mr297, mr297 != "nc")
 uq_q76<- filter(pv_q76, q76 != "nc")
 
-pv_q76<- select(uq_q76, snp_id, q76)#to extract list of pv alleles
-
-#count non coding total snp
-nc_tsnp<- sqldf("select * from tsnp where tsnp.snpID NOT IN (select snpID from ann_tsnp)")
-
 #extract unique snp from each variety
 snp[is.na(snp)]<- "NC"
 snp$countNC<- rowSums(snp == "NC")
@@ -65,6 +60,3 @@ ls_snp<- snp %>%
   filter(countNC == 5) %>%
   select(snpID, bali, ph, mrm16, q100, mr297, q76, countNC)
 
-ls_mr297<- ls_snp %>%
-  filter(mr297 != "NC") %>%
-  select(snpID, mr297)
