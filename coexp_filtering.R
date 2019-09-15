1d14dfd6378091e43b1d07e71a422709be1e6048
ruru-adra <rabiatul.adawiah83@gmail.com>
2019-09-15 15:39:53 +0800

    Update coexp_filtering.R

--- coexp_filtering.R
+++ coexp_filtering.R
@@ -1,28 +1,67 @@
-#create node-edge id
-ppan_fv$ne_OsID<- paste(ppan_fv$fv_OsID, ppan_fv$tf_OsID, sep="_")
-string_fv$ne_OsID<- paste(string_fv$node_OsID, string_fv$edge_OsID, sep="_")
-red_fv$ne_OsID<- paste(string_fv$node_OsID, string_fv$edge_OsID, sep="_")
+#read list of flavonoid/folate, coexpressed & TF
+fv<- read.table("fv.txt", header=T, sep="\t", quote="")
+ft<- read.table("fv.txt", header=T, sep="\t", quote="")
+tf<- read.table("tf_rice.txt", header=T, sep="\t", quote="")
+pcc<- read.table("pcc_svr.txt", header=T, sep="\t", quote="")
 
-s<- unique(select(string_fv, ne_OsID))
-p<- unique(select(ppan_fv, ne_OsID))
-r<- unique(select(red_fv, ne_OsID))
+#combine flavonoid/folate with tf data
+fv_tf<- rbind(fv, tf)#ensure col header sama
+ft_tf<- rbind(ft, tf)
+
+#filter list of flavonoid/folate, coexpressed & TF
+n_fv<- merge(fv, pcc, by="node")
+e_fv<- merge(fv, pcc, by="edge")
+pcc_fv- unique(rbind(n_fv, e_fv))
+
+#create interaction id rna-seq
+pcc_fv$ne_OsID<- paste(pcc_fv$node_OsID, pcc_fv$edge_OsID, sep="_")
+pcc_ft$ne_OsID<- paste(pcc_ft$node_OsID, pcc_ft$edge_OsID, sep="_")
+
+****************************************************************************************
+
+#select hub gene
+nwk_fv<- read.csv("nwk_analyzer_degree_grn_ft.csv")
+nwk_fv<- arrange(ann_nwk_analyzer, desc(Degree, PCC))
+
+****************************************************************************************
+
+#select TF-folate
+#create node-edge id
+ppan_ft$ne_OsID<- paste(ppan_ft$ft_OsID, ppan_ft$ft_OsID, sep="_")
+string_ft$ne_OsID<- paste(string_ft$node_OsID, string_fv$edge_OsID, sep="_")
+red_ft$ne_OsID<- paste(red_ft$node_OsID, red_ft$edge_OsID, sep="_")
+pcc_ft$ne_OsID<- paste(pcc_ft$node_OsID, pcc_ft$edge_OsID, sep="_")
+
+s<- unique(select(string_ft, ne_OsID))
+p<- unique(select(ppan_ft, ne_OsID))
+r<- unique(select(red_ft, ne_OsID))
+t<- unique(select(pcc_ft, ne_OsID))
 
 s$type="string"
 p$type="ppan"
 r$type="red"
+t$type="rseq"
 
 #create total node-edge id genotype format
-grn_fv<- Reduce(function(x,y) merge(x, y, by = "ne_OsID", all.x = TRUE, all.y = TRUE),
-                  list(s,p,c,r))
+grn<- Reduce(function(x,y) merge(x, y, by = "ne_OsID", all.x = TRUE, all.y = TRUE),
+                  list(s,p,t,r))
 
 #count uniqe, sharing node-edge id
-grn<- grn_fv
 grn[is.na(grn)]<-"NC"
 grn$countNC<- rowSums(grn=="NC")#count occurance of NC
 fltr_grn<- filter(grn, countNC == 2)
 fltr_grn2<- filter(grn, countNC == 0)
 fltr_grn3<- filter(grn, countNC == 1)
 ptl_grn<- rbind(fltr_grn, fltr_grn2, fltr_grn3)
+
+****************************************************************************************
+
+#GO analysis
+entry_tf<- agrigo_tf %>%
+  select(GO_acc, entries) %>%
+  cSplit("entries", "// ") %>%
+  select_if(~!all(is.na(.))) %>%
+  filter(m_tf != " ")
 
 #transform wide to long
 t_tf<- t(m_tf)
@@ -38,3 +77,4 @@ write.table(clean_go_tf, "enr_go_reactome/clean_go_tf.txt", col.names=T, row.nam
 r_ft<-folate_reactome %>%
   mutate(OsID=strsplit(as.character(OsID), ";")) %>%
   unnest(OsID)
+
