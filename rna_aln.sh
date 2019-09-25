c0fd592df4dba6e9499af3c5e74aaac2bd60c7cd
ruru-adra <rabiatul.adawiah83@gmail.com>
2019-09-25 11:17:31 +0800

    Create rna_aln.sh

---
+++ rna_aln.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+tophat2 -p 4 -G nipponbare.gff3 -o aln_bali.out nipponbare rna_bali_1.fastq rna_bali_2.fastq
+tophat2 -p 4 -G nipponbare.gff3 -o aln_bali.out nipponbare rna_bali_1.fastq rna_bali_2.fastq
+tophat2 -p 4 -G nipponbare.gff3 -o aln_bali.out nipponbare rna_bali_1.fastq rna_bali_2.fastq
+tophat2 -p 4 -G nipponbare.gff3 -o aln_bali.out nipponbare rna_bali_1.fastq rna_bali_2.fastq
+tophat2 -p 4 -G nipponbare.gff3 -o aln_bali.out nipponbare rna_bali_1.fastq rna_bali_2.fastq
+tophat2 -p 4 -G nipponbare.gff3 -o aln_bali.out nipponbare rna_bali_1.fastq rna_bali_2.fastq
