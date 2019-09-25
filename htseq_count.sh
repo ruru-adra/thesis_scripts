23226bb0d35971ac0a328871e7a1b5d7d73f306d
ruru-adra <rabiatul.adawiah83@gmail.com>
2019-09-25 14:45:57 +0800

    Create htseq_count.sh

---
+++ htseq_count.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+htseq-count -s yes -r pos -t gene -i geneID -f bam aln_bali.bam nipponbare.gff > bali_counts.out
+htseq-count -s yes -r pos -t gene -i geneID -f bam aln_ph9.bam nipponbare.gff > ph9_counts.out
+htseq-count -s yes -r pos -t gene -i geneID -f bam aln_mrm16.bam nipponbare.gff > mrm16_counts.out
+htseq-count -s yes -r pos -t gene -i geneID -f bam aln_q100.bam nipponbare.gff > q100_counts.out
+htseq-count -s yes -r pos -t gene -i geneID -f bam aln_mr297.bam nipponbare.gff > mr297_counts.out
+htseq-count -s yes -r pos -t gene -i geneID -f bam aln_q76.bam nipponbare.gff > q76_counts.out
