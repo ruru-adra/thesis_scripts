78cde85d9f37fa1cb29ddec0550a82d903c2d66d
ruru-adra <rabiatul.adawiah83@gmail.com>
2019-09-25 11:59:30 +0800

    Create cufflinks_assembly.sh

---
+++ cufflinks_assembly.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cufflinks -p 8 -o bali_t_out aln_bali.bam
+cufflinks -p 8 -o ph9_t_out aln_ph9.bam
+cufflinks -p 8 -o mrm16_t_out aln_mrm16.bam
+cufflinks -p 8 -o q100_t_out aln_q100.bam
+cufflinks -p 8 -o mr297_t_out aln_mr297.bam
